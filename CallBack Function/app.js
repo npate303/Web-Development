a4990809cefe9f5621b2f348bdb1f92bd0c8cec6
Nidhi <nidhi@Nidhis-MacBook-Pro.local>
2022-01-17 03:24:51 -0600

    JavaScript Add clasList

--- CallBack Function/app.js
+++ CallBack Function/app.js
@@ -1,13 +1,76 @@
-const ul = document.querySelector('.people');
+const ul = document.querySelector(".people");
 
-const people = ['Nidhi' , 'Bina' , 'Riken', 'Nitya' , 'Isha', 'Tammna'];
+const people = ["Nidhi", "Bina", "Riken", "Nitya", "Isha", "Tammna"];
 
 let html = ``;
-people.forEach( (person) => 
-{ 
-    html += `<li style ="color:dimgray">${person}</li>` ;
-})
+people.forEach((person) => {
+  html += `<li style ="color:dimgray">${person}</li>`;
+});
 
 console.log(html);
 
-ul.innerHTML =html;
+ul.innerHTML = html;
+
+//-----------------------------------------------
+
+let user = {
+  name: "nidhi",
+  age: [25, 60, 80, 90],
+  logAge: function () {
+    this.age.forEach((number) => console.log(number));
+  },
+};
+
+console.log(user.name.toUpperCase());
+user.logAge();
+
+let ages = [1, 2, 3, 5];
+
+function nidhi() {
+  for (let i = 0; i < ages.length; i++) {
+    console.log(ages[i]);
+  }
+}
+
+nidhi();
+
+//lernt that all function in JavaScript returns undefiend
+//all function returns something at the end
+function r() {}
+
+console.log(r());
+
+//-----------------------------------------
+
+//This section is for DOM manipulation
+
+const paras = document.querySelectorAll("p");
+
+console.log(paras);
+
+paras.forEach((parasItem) => {
+  if (parasItem.textContent.includes("success")) {
+    //parasItem.setAttribute('style', 'color : green');
+    parasItem.classList.add("success");
+  } else if (parasItem.textContent.includes("error")) {
+    parasItem.setAttribute("style", "color : red");
+    //parasItem.classList.add('error')
+  }
+});
+
+// paras.forEach((parasItem) => {
+//   var x = parasItem.textContent;
+//   switch (x) {
+//     case parasItem.textContent.includes("success"):
+//       parasItem.setAttribute("style", "color : green");
+//       break;
+
+//     case parasItem.textContent.includes("error"):
+//       parasItem.setAttribute("style", "color : red");
+//       break;
+
+//     default:
+//       parasItem.setAttribute("style", "color : black");
+//   }
+// });
+
