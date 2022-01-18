11a764986245233d2a20f07d61066b2d4e336b6f
Nidhi <nidhi@Nidhis-MacBook-Pro.local>
2022-01-17 19:31:57 -0600

    event listner

--- Event Listner/app.js
+++ Event Listner/app.js
@@ -1,9 +1,31 @@
 //-- Event Listner --
-
+const ul = document.querySelector('ul');
 const listItem =  document.querySelectorAll('li');
+const button = document.querySelector('button');
 
-listItem.forEach( item => {
-    item.addEventListener( 'click', (e) => {
-        e.target.style.textDecoration = 'line-through';
-    })
+
+button.addEventListener( 'click' , ( ) => {
+   // ul.innerHTML += '<li>New list Item</li>';
+    //another way using create element
+    const li = document.createElement('li');
+    li.textContent = "something new";
+    ul.append(li);
+});
+
+// listItem.forEach( item => {
+//     item.addEventListener( 'click', (e) => {
+//        // e.target.style.textDecoration = 'line-through';
+//        //another way of removing an item is 
+//        e.target.remove();
+//     });
+// });
+
+//------------
+
+//---- Event Deligation ----
+
+ul.addEventListener('click', ( e ) => {
+    if(e.target.tagName === 'LI') {
+        e.target.remove();
+    }
 })
