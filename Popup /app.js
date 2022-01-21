0365bb80d31c5cffe24fbed698621650847f4c1c
Nidhi <nidhi@Nidhis-MacBook-Pro.local>
2022-01-21 01:49:57 -0600

    copying object

--- Popup /app.js
+++ Popup /app.js
@@ -2,6 +2,13 @@ const button = document.querySelector('button');
 const popup = document.querySelector('.popup-wrapper');
 const close = document.querySelector('.popup-close');
 
+//------------- concept of deep copying and shallow copying 
+a = { name : 'nidhi', age: 25};
+console.log(`i am coming from a ${a}`);
+b = a;
+console.log(`i am coming from b ${b}`);
+//--------------- concept of deep copying and shallow copying
+
 button.addEventListener('click', () => {
     popup.style.display = 'block';
 });
